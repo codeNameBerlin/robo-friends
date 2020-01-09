ca1ff11017f14fcc4184394fa2759134a793b151
egheCodes <ehioroboeghe@gmail.com>
2020-01-09 17:06:57 +0100

    Updates

---
+++ precache-manifest.43e025b5c00e18101ecbfe96d81571fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76bdd97f556d6c4671070bee6e031a4",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robo-friends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a6d6e62206479f8e5b62",
+    "url": "/robo-friends/static/css/main.e41a5f1c.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/robo-friends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "a6d6e62206479f8e5b62",
+    "url": "/robo-friends/static/js/main.410ca78b.chunk.js"
+  },
+  {
+    "revision": "395be69c958ddac3ab6b",
+    "url": "/robo-friends/static/js/runtime~main.f78bd4bc.js"
+  },
+  {
+    "revision": "155a0fc90bf6a7d30c76b487b90cac79",
+    "url": "/robo-friends/static/media/octin spraypaint a rg.155a0fc9.woff"
+  }
+]);
