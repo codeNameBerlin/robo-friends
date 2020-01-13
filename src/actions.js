a16c009611eb9abbb096631806df17f6105207f3
egheCodes <ehioroboeghe@gmail.com>
2020-01-13 14:46:18 +0100

    integrating redux

---
+++ src/actions.js
@@ -0,0 +1,18 @@
+import { 
+	CHANGE_SEARCH_FIELD,
+	REQUEST_REDUX_PENDING,
+	REQUEST_REDUX_SUCCESS,
+	REQUEST_REDUX_FAILED } from './constants.js';
+
+export const setSearchField = (text) => ({
+	type: CHANGE_SEARCH_FIELD,
+	payload: text
+});
+
+export const requestRobots = () => (dispatch) => {
+	dispatch({ type: REQUEST_REDUX_PENDING });
+	fetch('https://jsonplaceholder.typicode.com/users')
+	.then(response => response.json())
+	.then(data => dispatch({ type: REQUEST_REDUX_SUCCESS, payload: data }))
+	.catch(error => dispatch({ type: REQUEST_REDUX_FAILED, payload: error }))
+}
